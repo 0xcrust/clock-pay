febdeeff21cc984ba525e2642685f76a6b4a5719
crustydev <ademola1145@gmail.com>
2022-12-29 15:52:38 +0100

    Refactor program state

--- program/src/state.rs
+++ program/src/state.rs
@@ -1,21 +1,38 @@
 use borsh::{BorshSerialize, BorshDeserialize};
 use solana_program::pubkey::Pubkey;
 
+
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
-pub struct Allowance {
-    pub initializer: Pubkey,
+pub struct Accounting {
+    pub authority: Pubkey,
     pub mint: Pubkey,
-    pub receiver_token_account: Pubkey,
+    pub active_beneficiaries: Pubkey,
     pub vault: Pubkey,
-    pub cycles: u64,
-    pub cycle_start_time: i64,
-    pub amount: u64,
-    pub interval: u64,
-    pub cycle_paused: bool,
-    pub plan_paused: bool,
+    pub balance: u64,
+    pub bump: u8,
 }
 
-impl Allowance {
+impl Accounting {
+    pub fn size() -> usize {
+        std::mem::size_of::<Self>()
+    }
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct Payroll {
+    pub accounting: Pubkey,
+    pub receiver_token_account: Pubkey,
+    pub total_cycles: u64,
+    pub cycle_start_time: i64,
+    pub cycles_completed: u64,
+    pub amount: u64,
+    pub interval: u64,
+    pub bump: u8
+    //pub cycle_paused: bool,
+    //pub plan_paused: bool,
+}
+
+impl Payroll {
     pub fn size() -> usize {
         std::mem::size_of::<Self>()
     }
