9ff97a2a9784c343f30be24edefd923e4b8cfc44
crustydev <ademola1145@gmail.com>
2022-12-29 17:30:41 +0100

    Cargo fmt

--- program/src/entrypoint.rs
+++ program/src/entrypoint.rs
@@ -1,7 +1,5 @@
 use solana_program::{
-    account_info::AccountInfo,
-    pubkey::Pubkey,
-    entrypoint, entrypoint::ProgramResult,
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey,
 };
 
 use crate::processor;
@@ -10,7 +8,7 @@ entrypoint!(process_instruction);
 pub fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
-    instruction_data: &[u8]
+    instruction_data: &[u8],
 ) -> ProgramResult {
     processor::Processor::process(program_id, accounts, instruction_data)
 }
--- program/src/lib.rs
+++ program/src/lib.rs
@@ -1,7 +1,7 @@
 pub mod error;
-pub mod state;
 pub mod instruction;
 pub mod processor;
+pub mod state;
 
 #[cfg(not(feature = "no-entrypoint"))]
 pub mod entrypoint;
