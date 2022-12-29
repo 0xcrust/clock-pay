77ebfc654725277ca3f21d52125519295a4d73cb
crustydev <ademola1145@gmail.com>
2022-12-29 16:21:06 +0100

    Split logic and redefine instruction enum

--- program/src/instruction.rs
+++ program/src/instruction.rs
@@ -1,32 +1,61 @@
-use solana_program::program_error::ProgramError;
+use solana_program::{
+    pubkey::Pubkey,
+    program_error::ProgramError
+};
 use borsh::BorshDeserialize;
 use crate::error::ClockPayError::InvalidInstruction;
 
 
 pub enum ClockPayInstruction {
-    /// Initializes an allowance plan
+    /// Initializes an accounting instance. Represents the user's global state.
     ///
     ///
     /// Accounts expected:
     ///
-    /// 0. `[signer, writable]` The initializer's system account.
-    /// 1. `[writable]` The receiver's account.
-    /// 2. `[writable]` The program account that stores the state. A pda with seeds &[b"allowance".as_ref(), 
-    ///     initializer.key.as_ref(), receiver.key.as_ref()]
-    /// 3. `[]` Token mint
-    /// 4. `[writable]` Initializer token account.
-    /// 5. `[]` The vault: A pda token account seeded from the state address. seeds: &[b"vault".as_ref(), state.key.as_ref()]
-    /// 6. `[]` The System Program
-    /// 7. `[]` The Token Program
-    InitAllowance(InitArgs),
+    /// 0. `[signer, writable]` The initializer.
+    /// 1. `[]` Token mint
+    /// 2. `[writable]` The program account that stores the state. A pda with seeds &[b"account".as_ref(), initializer.key.as_ref()]
+    /// 4. `[]` The vault: Associated token account for the state pda
+    /// 5. `[]` The System Program
+    /// 6. `[]` The Token Program
+    InitAccounting{},
+    /// Deposits into the Accounting vault
+    ///
+    ///
+    /// Accounts expected:
+    ///
+    /// 0. `[signer, writable]` The authority of the Accounting state account.
+    /// 1. `[writable]` The Accounting state account.
+    /// 2. `[writable]` The authority's token account to be debited.
+    /// 3. `[writable]` The vault
+    /// 4. `[]` The Token Program,
+    Deposit(DepositArgs),
+    /// Adds a beneficiary to the payroll
+    ///
+    ///
+    /// Accounts expected
+    /// 
+    /// 1. `[signer]` The authority of the Accounting state instance.
+    /// 2. `[writable]` The Accounting state account.
+    /// 3. `[writable]` The vault to be withdrawn from.
+    /// 4. `[writable]` The receiver's token account.
+    /// 5. `[]` The token program
+    /// 6. `[]` Clockwork(I haven't figured this out yet)
+    NewPayroll(StartPayArgs),
 }
 
 #[derive(BorshDeserialize, Debug)]
-pub struct InitArgs {
-    pub time_to_start: u64,
-    pub amount: u64,
-    pub cycles: u64,
-    pub interval: u64,
+struct DepositArgs {
+    amount: u64,
+}
+
+#[derive(BorshDeserialize, Debug)]
+struct StartPayArgs {
+    time_till_start: u64,
+    amount: u64,
+    cycles: u64,
+    interval: u64,
+    receiver_key: Pubkey,
 }
 
 impl ClockPayInstruction {
@@ -34,13 +63,26 @@ impl ClockPayInstruction {
         let (tag, rest) = input.split_first().ok_or(InvalidInstruction)?;
 
         Ok(match tag {
-            0 => Self::unpack_init_args(rest)?,
+            0 => Self::unpack_init_accounting_args()?,
+            1 => Self::unpack_deposit_args(rest)?,
+            2 => Self::unpack_new_payroll_args(rest)?,
             _ => return Err(InvalidInstruction.into()),
         })
     }
 
-    pub fn unpack_init_args(src: &[u8]) -> Result<Self, ProgramError> {
-        let unpacked = InitArgs::try_from_slice(src)?;
-        Ok(ClockPayInstruction::InitAllowance(unpacked))
+    fn unpack_init_accounting_args() -> Result<Self, ProgramError> {
+        Ok(Self::InitAccounting{})
     }
+
+    fn unpack_deposit_args(src: &[u8]) -> Result<Self, ProgramError> {
+        let unpacked_args = DepositArgs::try_from_slice(&src)?;
+        Ok(Self::Deposit(unpacked_args))
+    }
+
+    fn unpack_new_payroll_args(src: &[u8]) -> Result<Self, ProgramError> {
+        let unpacked_args = StartPayArgs::try_from_slice(&src)?;
+        Ok(Self::NewPayroll(unpacked_args))
+    }
+
+
 }
