648d4d79316075d9e497b90c501bf15198ef1fde
crustydev <ademola1145@gmail.com>
2023-01-03 21:24:11 +0100

    Edit Payroll state fields

--- program/src/state.rs
+++ program/src/state.rs
@@ -8,27 +8,29 @@ pub struct Accounting {
     pub active_payrolls: u64,
     pub vault: Pubkey,
     pub balance: u64,
+    pub active: bool,
     pub bump: u8,
 }
 
 impl Accounting {
-    pub const SIZE: usize = 113;
+    pub const SIZE: usize = 32 + 32 + 8 + 32 + 8 + 1 + 1;
 }
 
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
 pub struct Payroll {
     pub accounting: Pubkey,
-    pub receiver_token_account: Pubkey,
-    pub total_cycles: u64,
-    pub cycle_start_time: i64,
-    pub cycles_completed: u64,
-    pub total_amount_paid: u64,
+    pub active: bool,
     pub amount: u64,
-    pub interval: u64,
-    pub bump: u8, //pub cycle_paused: bool,
-                  //pub plan_paused: bool,
+    pub total_amount_disbursed: u64,
+    pub cron_schedule: [u8; 30],
+    pub receiver: Pubkey,
+    pub max_cycles: u64,
+    pub cycles_completed: u64,
+    pub thread: Pubkey,
+    pub bump: u8,
 }
 
 impl Payroll {
-    pub const SIZE: usize = 113;
+    pub const SCHEDULE_LEN: usize = 30;
+    pub const SIZE: usize = 32 + 1 + 8 + 8 + (30) + 32 + 8 + 8 + 32 + 1;
 }
